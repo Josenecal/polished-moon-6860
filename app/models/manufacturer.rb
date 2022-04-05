48ab88f95bea7844e70b2323ea5c7d472058f1ef
Joseph Senecal <jasenecal2@gmail.com>
2022-04-05 16:10:59 -0600

    Feat: manufacturer model method ::map_items

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -2,4 +2,12 @@ class Manufacturer < ApplicationRecord
   validates_presence_of :name, :location
   has_many :manufacturer_items
   has_many :items, through: :manufacturer_items
+
+  def self.map_items # WOW this is a heavy method
+    products = {}
+    all.each do |manufacturer|
+      products[manufacturer] = manufacturer.items
+    end
+    products
+  end
 end
--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -10,5 +10,26 @@ RSpec.describe Manufacturer, type: :model do
     it { should have_many :manufacturer_items }
     it { should have_many(:items).through(:manufacturer_items) }
   end
-    
+
+  describe 'class methods' do
+    it "::map_items returns a hash, all manufacturers as keys mapped to associations of their items" do
+      project = Project.create!(name: "Food Sales", manager: "Bill")
+      manufacturer_1 = Manufacturer.create!(name: "La Colombe", location: "Philladelphia, PA")
+      manufacturer_2 = Manufacturer.create!(name: "Lovin Oven", location: "Boulder, CO")
+      item_1 = project.items.create!(name: "Monaco, 12oz", cost: 8)
+      item_2 = project.items.create!(name: "House Blend, 12oz", cost: 8)
+      item_3 = project.items.create!(name: "Gluten Free Blueberry Muffin", cost: 4)
+      join_1 = ManufacturerItem.create!(item_id: item_1.id, manufacturer_id: manufacturer_1.id)
+      join_2 = ManufacturerItem.create!(item_id: item_2.id, manufacturer_id: manufacturer_1.id)
+      join_3 = ManufacturerItem.create!(item_id: item_3.id, manufacturer_id: manufacturer_2.id)
+
+      expected = Manufacturer.map_items
+# binding.pry
+      expect(expected).to eq({
+        manufacturer_1 => manufacturer_1.items,
+        manufacturer_2 => manufacturer_2.items
+      })
+    end
+  end
+
 end
