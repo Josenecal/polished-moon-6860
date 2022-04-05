0760e2045df0a16d45da5ed5e5c20ba60039c67f
Joseph Senecal <jasenecal2@gmail.com>
2022-04-05 17:04:44 -0600

    Feat: project model method #average_cost

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ApplicationRecord
   validates_presence_of :name, :manager
   has_many :items
+
+  def average_cost
+    count = items.length.to_f
+    sum = items.sum {|item| item.cost}.to_f
+    average = (sum / count).round(2)
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Project, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'instance methods' do
+
+    it '#average_cost' do
+      project = Project.create!(name: "Bedding", manager: "Allon")
+      item_1 = project.items.create!(name: "standard pillow", cost: 3)
+      item_2 = project.items.create!(name: "throw pillow", cost: 2)
+      item_3 = project.items.create!(name: "twin sheets", cost: 4)
+      item_4 = project.items.create!(name:  "standard pillowcase", cost: 1)
+
+      expect(project.average_cost).to eq(2.50)
+    end
+  end
 end
