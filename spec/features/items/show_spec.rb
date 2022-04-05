26e5ecad160d4a34f4d46841da2426bf93fbb82c
Joseph Senecal <jasenecal2@gmail.com>
2022-04-05 16:33:02 -0600

    Feat: item show page displays number of manufacturers

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe "Item Show" do
     expect(page).to have_content(item.cost)
     expect(page).to have_content(item.name)
   end
+
+  it "displays a count of the manufacturers who make this item" do
+    project = Project.create!(name: "Food Sales", manager: "Bill")
+    manufacturer_1 = Manufacturer.create!(name: "La Colombe", location: "Philladelphia, PA")
+    manufacturer_2 = Manufacturer.create!(name: "Lovin Oven", location: "Boulder, CO")
+    manufacturer_3 = Manufacturer.create!(name: "Bake House", location: "Aurora, CO")
+    item_2 = project.items.create!(name: "House Blend, 12oz", cost: 8)
+    item_3 = project.items.create!(name: "Gluten Free Blueberry Muffin", cost: 4)
+    join_2 = ManufacturerItem.create!(item_id: item_2.id, manufacturer_id: manufacturer_1.id)
+    join_3 = ManufacturerItem.create!(item_id: item_3.id, manufacturer_id: manufacturer_2.id)
+    join_4 = ManufacturerItem.create!(item_id: item_3.id, manufacturer_id: manufacturer_3.id)
+
+    visit "/items/#{item_2.id}"
+
+    expect(page).to have_content("Manufacturers: 1")
+
+    visit "/items/#{item_3.id}"
+
+    expect(page).to have_content("Manufacturers: 2")
+  end
 end
